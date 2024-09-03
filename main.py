51038f5c628ad83a6acd631104d1ec5e71dc3532
dartagnan sams <dartagnansams1@icloud.com>
2024-09-03 00:41:16 -0400

    added eraser functionality,flipped the camera frame

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class Game:
         self.mphands = mp.solutions.hands
         self.hands = self.mphands.Hands(max_num_hands=1)
         self.mp_drawing = mp.solutions.drawing_utils
+        self.window_width,self.window_height = 700,500
+        self.drawing_mode = False
+        self.eraser_size = 20
+        self.pencil_size = 15
         
 
     
@@ -25,12 +29,9 @@ class Game:
                 cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),5) #draws a rectangle on the detected face boundary
                 self.face_center_x,self.face_center_y = int(x+w/2),int(y+h/2)
                 # print(int(face_center_x),int(face_center_y))
-                self.points.append((int(self.face_center_x),int(self.face_center_y)))
+                # self.points.append((int(self.face_center_x),int(self.face_center_y)))
                 # cv.circle(frame,(self.face_center_x,self.face_center_y),10,(0,0,255),-1) #draw a circle at the face center point
 
-
-    def draw_circle(self,frame,pos,thickness,color):
-        return cv.circle(frame,pos,thickness,color,-1)
     
     def find_distance(self,obj1_pos,obj2_pos): #distance formula sqrt((x2-x1)**2+(y2-y1)**2)
         try:
@@ -60,11 +61,16 @@ class Game:
                 ringip = hand_landmarks.landmark[16]
                 pinkietip = hand_landmarks.landmark[20]
                 finger_distance = self.find_distance(indextip,middletip)
-                print(finger_distance)
+                # print(f"Finger distance is {finger_distance}")
                 # print(thumbtip.x * self.w,thumbtip.y * self.h) #have to multiply the initial (x,y) coords by the window shape width and height to get proper coordinates if not the coords are on a 0-1 scale 
-                
-                if finger_distance < 0.1: #if two fingers are close together then can start drawing 
-                    self.points.append((int(indextip.x*self.w),int(indextip.y*self.h))) #adds localtion of index finger (x,y) coords to a list to draw circles later giving the effect of a pencil
+                pos = (int(indextip.x * self.w), int(indextip.y * self.h))
+
+                if finger_distance > 0.19 and self.drawing_mode: #if only holding up 1 finger while the others are down 
+                    self.points.append(pos) #adds localtion of index finger (x,y) coords to a list to draw circles later giving the effect of a pencil
+
+                if finger_distance < 0.1: #logic to erase drawing points
+                    cv.circle(self.frame2,pos,self.eraser_size,(255,255,255),1) #displays a visual effect to show user is erasing points
+                    self.points = [p for p in self.points if self.find_distance(p, pos) > self.eraser_size] #only keeps the pencil drawings from the list if they are greater than the eraser size
 
 
             # hand_landmarks = found_hands.multi_hand_landmarks[0]
@@ -79,7 +85,8 @@ class Game:
         while True:
             self.ret,self.frame=camera.read()
             self.h,self.w,self.c = self.frame.shape
-            self.gray=cv.cvtColor(self.frame,cv.COLOR_BGR2RGB)
+            self.gray=cv.cvtColor(self.frame,cv.COLOR_BGR2GRAY)
+            self.frame2 = self.frame.copy()
             # self.detect(self.frame,self.gray,"face")
             # x1,y1 = 10,50
             # x2,y2 = 90,80
@@ -90,15 +97,32 @@ class Game:
             # print(self.find_distance([x1,y1],[self.face_center_x,self.face_center_y]))
             # self.draw_contours(self.gray)
             for p in self.points:
-                cv.circle(self.gray,(p[0],p[1]),10,(0,0,255),-1) #draw a circle from a list of coords (x,y)
+                cv.circle(self.frame2,(p[0],p[1]),self.pencil_size,(0,0,255),-1) #draw a circle from a list of coords (x,y)
 
             self.draw_hand_skeleton()
             
             try:
-                cv.imshow('Gray Window',self.gray)
+                #flip the frame (easier to visually understand)
+                self.frame = cv.flip(self.frame,1)
+                self.frame2 = cv.flip(self.frame2,1)
+
+
+                cv.imshow('Gray Window',self.frame2)
                 cv.imshow('Window',self.frame)
                 if cv.waitKey(1)==ord('q'):
                     break
+
+                if cv.waitKey(1)==ord('1'):
+                    print("clearing all")
+                    self.points.clear()
+
+                if cv.waitKey(1)==ord('2'): 
+                    print("you can now draw")
+                    self.drawing_mode = True
+
+                if cv.waitKey(1)==ord('3'): 
+                    print("disabling drawing mode")
+                    self.drawing_mode = False
             
             except:
                 print('Camera error')
